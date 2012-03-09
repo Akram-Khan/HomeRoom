8f0fe0544a3cf888f373d8d3fdf9bf0dec99ef93
Akram Khan <akramkhan83@gmail.com>
2012-03-10 00:17:26 +0530

    homepage draft

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Homeroom::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.action_mailer.default_url_options = { :host => 'homeroom.com' }
+  config.action_mailer.default_url_options = { :host => 'stormy-sunset-5295.heroku.com' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
