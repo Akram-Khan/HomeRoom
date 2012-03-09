af9cc38ff08d92c33d6a263457e32a8daf67dbe8
Akram Khan <akramkhan83@gmail.com>
2012-03-09 16:02:08 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeroom::Application.config.secret_token = '373a6076534f7e4a5dd70c13c2914534a10268ed53216d779f674627b01d16102ca4b7509a78991b0496a0deeda3eb3cdbe425001fc884d334f600b5e3056543'
