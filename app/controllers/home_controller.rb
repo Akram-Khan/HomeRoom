f47679d290f73224135a7c97e71516dc2f3f0b8a
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-03-21 13:03:02 +0530

    duplicated students, teachers fixed, course teachers view made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
-  def index
-  end
+	before_filter :redirect_if_logged_in
 
+	def index
+	end
+
+	private
+
+	def redirect_if_logged_in
+		if user_signed_in?
+			redirect_to dashboard_path
+		end
+	end
 end
