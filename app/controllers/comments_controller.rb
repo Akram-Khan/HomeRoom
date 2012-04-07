16a51e771e2f775d4a99077ef0c89f8167a520b6
Akram Khan <akramkhan83@gmail.com>
2012-04-07 19:49:19 +0530

    note functionality and comment done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
 	def create
 	    @note = Note.find(params[:comment][:note_id])
-	    @comment = @note.comments.new(params[:comment], :user_id =>current_user.id)
+	    @comment = @note.comments.new(:comment=> params[:comment][:comment], :user_id =>current_user.id)
 	    @comment.save
 	    redirect_to :back  
   	end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :note
+	belongs_to :user
 end
+
