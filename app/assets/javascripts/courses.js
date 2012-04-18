f44fd9886e86bf88c40abae2099ca029b2c18ef6
Akram Khan <akramkhan83@gmail.com>
2012-04-18 20:59:48 +0530

    improvements in courses show

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-	$('.note').click(function(){
-		$('.note-form').fadeIn(1000);
+	
+	$('.show-comments').click(function(){
+		$(this).parent('.note-comment-link').nextAll('.custom-comment').slideToggle();
+	}).toggle(function(){
+		$(this).children('span').children('a').html("&#8211").css('fontWeight', 'bold');
+		} , function() {
+		$(this).children('span').children('a').text("+").css('fontWeight', 'bold');
 	});
 
-	$('.note-comment-link').click(function(){
-		$('.note-comment-form').fadeIn(1000);
-	});
 });
+
+
+/*
+$(document).ready(function(){
+	$('.note-comment-link').click(function(){
+		$(this).nextAll('.custom-comment').eq(0).toggle();
+	}).toggle(function(){
+		$(this).children('span').text("-");
+		} , function() {
+		$(this).children('span').text("+");
+	});
+
+});
+*/
